894eb2e239802cc33a75605fef4d0d58ea03b9b6
yillkid <yillkid@gmail.com>
2024-07-23 23:46:07 +0800

    post

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
 
+// 使用 express.json() 中間件來解析 JSON 格式的請求體
+app.use(express.json());
+
 // 根路徑的 GET 請求處理
 app.get('/', function(req, res) {
   res.send('<h1>Hello</h1> Express');
 });
 
-// /sensor 路徑的 GET 請求處理，帶查詢參數
-app.get('/sensor', function(req, res) {
-  // 獲取查詢參數
-  const temperature = req.query.temperature || 'N/A';
-  const humidity = req.query.humidity || 'N/A';
+// /sensor 路徑的 POST 請求處理，接收請求體中的 JSON 數據
+app.post('/sensor', function(req, res) {
+  // 獲取請求體中的數據
+  const temperature = req.body.temperature || 'N/A';
+  const humidity = req.body.humidity || 'N/A';
   
   // 準備回應數據
   const data = {
